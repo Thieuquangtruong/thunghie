c6e1478ec1f177ee02aa0ca8eea409df7c282915
truong <thieuquangtruong3861@gmail.com>
2022-12-18 18:34:25 +0700

    tutorial

---
+++ script.js
@@ -0,0 +1,29 @@
+// fuction loading
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+  
+  function fadeOut(){
+    setInterval(loader, 2500);
+  }
+  
+  window.onload = fadeOut();
+
+  let menu =document.querySelector('#menu-bar');
+  let navbar = document.querySelector('.navbar');
+// input back home
+  menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active')
+  }
+  window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 60){
+        document.querySelector('#scroll-top').classList.add('active');
+      }else{
+        document.querySelector('#scroll-top').classList.remove('active');
+      }
+    
+    }
